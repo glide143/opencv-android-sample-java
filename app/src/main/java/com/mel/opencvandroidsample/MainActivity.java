1261600732da6711e72da793f587934e2fea2472
melchizedek <melchizedek30@gmail.com>
2021-11-19 15:17:03 +0800

    Initial Commit

---
+++ app/src/main/java/com/mel/opencvandroidsample/MainActivity.java
@@ -0,0 +1,114 @@
+package com.mel.opencvandroidsample;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import static android.view.View.VISIBLE;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private static final String TAG = MainActivity.class.getName();
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                mOpenCvCameraView.enableView();
+            } else {
+                super.onManagerConnected(status);
+            }
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.CAMERA }, 1);
+
+        mOpenCvCameraView = findViewById(R.id.camView);
+        mOpenCvCameraView.setVisibility(VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_ANY);
+
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        Log.d(TAG, "called onCameraViewStarted");
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        Log.d(TAG, "called onCameraViewStopped");
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if ( mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(!OpenCVLoader.initDebug()) {
+            Log.e("OpenCV", "Unable to load OpenCV!");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this,
+                    mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV loaded Successfully!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String []permissions,
+            int[] grantResults) {
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mOpenCvCameraView.setCameraPermissionGranted();
+            } else {
+                Toast.makeText(this, "Camera permission required.",
+                        Toast.LENGTH_LONG).show();
+                this.finish();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# OpenCV Android Sample On Java
+
+A simple android project that uses the library 
+**[OpenCV Android](https://github.com/quickbirdstudios/opencv-android)**
+
+## 💻 Sample output
+![alt text](https://i.imgur.com/iR1Q7H2.png)
+
